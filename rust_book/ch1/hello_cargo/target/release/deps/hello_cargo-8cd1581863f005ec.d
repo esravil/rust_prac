db2e6ec12401453ae4d6c840ae9a67ab96c40a00
esravil <esravil99@gmail.com>
2025-05-19 12:38:03 -0400

    finishing up ch1 exercises

---
+++ rust_book/ch1/hello_cargo/target/release/deps/hello_cargo-8cd1581863f005ec.d
@@ -0,0 +1,5 @@
+/Users/nahi/repos/Rust Exercises/rust_book/ch1/hello_cargo/target/release/deps/hello_cargo-8cd1581863f005ec: src/main.rs
+
+/Users/nahi/repos/Rust Exercises/rust_book/ch1/hello_cargo/target/release/deps/hello_cargo-8cd1581863f005ec.d: src/main.rs
+
+src/main.rs:
---
+++ rust_book/ch1/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/nahi/repos/Rust\ Exercises/rust_book/ch1/hello_cargo/target/release/hello_cargo: /Users/nahi/repos/Rust\ Exercises/rust_book/ch1/hello_cargo/src/main.rs
