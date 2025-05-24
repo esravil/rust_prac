4732e7d3be7788ecba4469e7a35de475ae0fd6d7
esravil <esravil99@gmail.com>
2025-05-24 15:02:16 -0400

    rust 1.2.3 vec fmt

--- rust_book/ch1/format_vec/src/main.rs
+++ rust_book/ch1/format_vec/src/main.rs
@@ -1,31 +1,39 @@
 use std::fmt;
 
+// define a new struct, for vectors named list
 struct List(Vec<i32>);
+// dont presume that debug print will work on the list, but will need to further check in tests
+
+// implement the fmt::Display here
 
 impl fmt::Display for List {
 
+    // nested fmt fxn
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 
-        let vec = &self.0; // since we want the items in the vactor, not the vector object itself, os we can iter all the values of the vector
-
-        write!(f, "[")?; // why ? idk
+        // instantiate a struct here
+        let vec = &self.0; // since we are grabbing the param from the vector items itself,
+        // need to add a portion where we indicate that we are accessing the class var, or vec vars
+        
+        // write opening bracket
+        write!(f, "[")?;
 
         for (i, v) in vec.iter().enumerate() {
 
-            if i != 0 { write!(f, ", ")?; } // the ? denotes we will print multiple of these out
-
+            if i != 0 { write!(f, ", ")?; }
             write!(f, "{}: {}", i, v)?;
 
         }
 
-        write!(f, "]")
-
+        write!(f, "]") // doesnt need closing semi or the ? param, indicates end of all args to be passed through
     }
+
 }
 
+// main fn
 fn main() {
 
-    let v = List(vec![1, 2, 3]); // for instantiating a vector, need to have the struct if applicable, then the vec! with the array inside
-    println!("{}", v);
+    let vector = List(vec![1, 2, 3]);
+    println!("{}", vector);
 
 }
